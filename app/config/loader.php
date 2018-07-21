c5e7ff515e03a44a6d63b1522bc8b99d8bf6ef41
vagrant <vagrant@phalcon.local>
2018-07-21 10:27:02 +0000

    Base Project

---
+++ app/config/loader.php
@@ -0,0 +1,23 @@
+<?php
+
+use Phalcon\Loader;
+
+$loader = new Loader();
+
+/**
+ * Register Namespaces
+ */
+$loader->registerNamespaces([
+    'Shop_categories\Models' => APP_PATH . '/common/models/',
+    'Shop_categories'        => APP_PATH . '/common/library/',
+]);
+
+/**
+ * Register module classes
+ */
+$loader->registerClasses([
+    'Shop_categories\Modules\Frontend\Module' => APP_PATH . '/modules/frontend/Module.php',
+    'Shop_categories\Modules\Cli\Module'      => APP_PATH . '/modules/cli/Module.php'
+]);
+
+$loader->register();
---
+++ app/modules/frontend/controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Shop_categories\Modules\Frontend\Controllers;
+
+class IndexController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+
+    }
+
+}
+
