243bd87c8e23ea24631a1dd90deb8e1e9a9cabaf
wajdi <creations.forum@gmail.com>
2021-05-15 12:34:18 +0300

    Fix migration command

--- utilities/docker-entrypoint.sh
+++ utilities/docker-entrypoint.sh
@@ -8,7 +8,7 @@ echo "Run migrations ..."
 if [[ "${UNIT_TEST}" ]]; then
   export MYSQL_DATABASE=shop_categories_test
   export MONGODB_DATABASE=shop_categories_test
-  phalcon *migration run --migrations=tests/migrations/
+  phalcon migration run --migrations=tests/migrations/
 else
   phalcon migration run --migrations=app/migrations/
 fi
